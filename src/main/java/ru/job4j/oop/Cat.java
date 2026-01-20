930395ceb0cc03b1e3b055ba3bf431871f48f5e7
AlexNosovZ <zzlifnr@gmail.com>
2026-01-21 00:28:00 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,6 +1,21 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String name;
+    private String food;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void show() {
+        System.out.println(this.name + " " + this.food);
+    }
+
     public static void main(String[] args) {
         Cat peppy = new Cat();
         Cat sparky = new Cat();
